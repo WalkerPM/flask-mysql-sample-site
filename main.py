db4c8e3970338b37460deb863406adb7259674b3
WalkerPM <Minefirst007@gmail.com>
2021-11-20 01:59:23 +0000

    Templates fix

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ import pymysql
 
 app = Flask(__name__)
 
-@app.route('/')
-def main_page():
+def get_all():
+    list = []
+    con = pymysql.connect(host='localhost', user='admin', password='P@ssw0rd', database='db2')
     with con:
         cursor = con.cursor()
         cursor.execute('SELECT * from cars;')
         list = cursor.fetchall()
-    return render_template('enumerate.html', listing = list )
+    return list
+
+@app.route('/fetch-all')
+def fetchall_page():
+    return render_template('enumerate.html', listing = get_all() )
+@app.route("/")
+def main_page():
+    return render_template('main-page.html')
 
 
 if __name__ == "__main__":
-    
- 
-    con = pymysql.connect(host='localhost', user='admin', password='P@ssw0rd', database='db2')
     app.run(port = 1488)
